527e41c203a2f974a240c176230b7e10c7aa1334
akramoo <samirounis2@gmail.com>
2025-02-15 00:21:57 +0100

    feat: config tailwind css project

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,9 +8,27 @@ export default {
   ],
   theme: {
     extend: {
+      backgroundImage: {
+        'gradient-custom': "linear-gradient(180deg, rgba(255, 255, 255, 0.4) 40%, rgba(217, 217, 217, 0.2) 20%)",
+      },
+      fontFamily: {
+        lato: ["Lato", "sans-serif"],
+      },
       colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
+        primary: "#FFD700", // Gold (Example, replace with extracted colors)
+        secondary: "#FF4500", // Orange-Red
+        background: "#1A1A1A", // Dark Gray
+        accent: "#00BFFF", // Deep Sky Blue
+      },
+      fontSize: {
+        xs: "5px",
+        sm: "19px",
+        base: "28px",
+        lg: "39px",
+        xl: "53px",
+        "2xl": "87px",
+        "3xl": "178px",
+        "4xl": "482px",
       },
     },
   },
